320fc10e35a60b781d14214ee8f72db9329a1ebe
Caleb Stouter <cdstouter@gmail.com>
2017-04-11 23:33:30 -0400

    moved utility functions into a separate header

--- main.cpp
+++ main.cpp
@@ -1,34 +1,12 @@
 #include <sndfile.h>
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <vector>
 #include <boost/program_options.hpp>
 
+#include "utilities.hpp"
+
 namespace po = boost::program_options;
-
-float calculateRMS(float *data, long long samples) {
-    float mean = 0.0;
-    for (long long i = 0; i<samples; i++) {
-        float square = pow(data[i], 2.0);
-        mean += square;
-    }
-    mean = pow(mean / (float)samples, 0.5);
-    return mean;
-}
-
-float VtoDB(float v) {
-    return 20.0f * log10(v);
-}
-
-float DBtoV(float db) {
-    return pow(10.0, db / 20.0);
-}
-
-struct gainPoint {
-    float gain;
-    sf_count_t position;
-};
 
 int main(int argc, char **argv) {
     std::string inputFilename = "";
